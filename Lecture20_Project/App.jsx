c3dbba8204fcb190a9ca108ddff9936fc65f4637
Brijesh <brijeshprajapati2022@gmail.com>
2026-07-01 17:57:59 +0530

    final project

--- Lecture20_Project/App.jsx
+++ Lecture20_Project/App.jsx
@@ -1,6 +1,6 @@
 import Header from "./components/Header";
 import { Outlet } from "react-router";
-import './app.css'
+import './App.css'
 import {  ThemeProvider } from "./contexts/ThemeContext";
 
 const App = () => {  
