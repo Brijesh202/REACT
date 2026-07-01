7c7001129ed871e11e6c956c8589bf1f4e91b8c0
Brijesh <brijeshprajapati2022@gmail.com>
2026-07-01 19:18:16 +0530

    Expense tracker form

---
+++ Lecture32_Forms/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import './App.css'
+import { ExpenseForm } from './components/ExpenseForm'
+import { ExpenseTable } from './components/ExpenseTable'
+import expenseData from './expenseData'
+
+function App() {
+  const [expenses, setExpenses] = useState(expenseData)
+
+  return (
+   <>
+    <main>
+      <h1>Track Your Expense</h1>
+      <div className="expense-tracker">
+        <ExpenseForm setExpenses={setExpenses}/>
+        <ExpenseTable expenses={expenses}/>
+        <div className="context-menu">
+            <div>Edit</div>
+            <div>Delete</div>
+        </div>
+      </div>
+    </main>
+   </>
+  )
+}
+
+export default App
