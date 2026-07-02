19944dbdba168ed2e152772ce31a05aaa5592ad5
Brijesh <brijeshprajapati2022@gmail.com>
2026-07-02 22:41:12 +0530

    Add filter functionality using simple logic

--- Lecture32_Forms/src/components/ExpenseTable.jsx
+++ Lecture32_Forms/src/components/ExpenseTable.jsx
@@ -1,13 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const ExpenseTable = ({expenses}) => {
+
+  const [category, setCategory] = useState('')
+
+  const filteredData = expenses.filter((expenses) => {
+    return expenses.category.toLowerCase().includes(category)
+  })
+  console.log(filteredData)
+
   return (
      <table className="expense-table">
           <thead>
             <tr>
               <th>Title</th>
               <th>
-                <select>
+                <select onChange={(e) => setCategory(e.target.value.toLowerCase())}>
                   <option value="">All</option>
                   <option value="grocery">Grocery</option>
                   <option value="clothes">Clothes</option>
@@ -47,12 +55,13 @@ export const ExpenseTable = ({expenses}) => {
           </thead>
           <tbody>
             {
-                expenses.map(({id,title,category,amount}) => {
-                    return <tr key={id}>
-                                <td>{title}</td>
-                                <td>{category}</td>
-                                <td>₹{amount}</td>
-                            </tr>
+                filteredData.map(({id,title,category,amount}) => {
+                    return (
+                      <tr key={id}>
+                        <td>{title}</td>
+                        <td>{category}</td>
+                        <td>₹{amount}</td>
+                      </tr>)
                 })
             }
             
