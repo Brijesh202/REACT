56aed1854c5072b9a90e502191d6792ce5c5c169
brijesh202 <brijeshprajapati2022@gmail.com>
2026-08-16 17:54:44 +0530

    Pop-up using React Portal

--- Lecture44_TailwindCSS_With_React/src/components/Contact.jsx
+++ Lecture44_TailwindCSS_With_React/src/components/Contact.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Modal from './Modal'
 
 export default function Contact(){
+    const [isOpen, setIsOpen] = useState(false)
     return(
-        <h1 className='text-xl'>Connect with us on {''} <a className="hover:text-blue-600" href="https://www.youtube.com/@procodrr" target='_blank'>Youtube</a></h1>
+        <>
+          <h1 className='text-xl'>Connect with us on {''} <a className="hover:text-blue-600" href="https://www.youtube.com/@procodrr" target='_blank'>Youtube</a></h1>
+          <button onClick={() => setIsOpen(true)}>Open Popup</button>
+          <Modal isOpen={isOpen} setIsOpen={setIsOpen} 
+            header={<p className='text-red-400 font-bold text-xl'>Hii</p>}
+            footer={<div className='flex justify-end'>
+                <button onClick={() => setIsOpen(false)} className='font-bold bg-slate-300 px-4 py-1 rounded'>Ok</button>
+                </div>}>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis, impedit! Velit non exercitationem, vero earum delectus facilis? Dicta repellendus accusamus mollitia quis delectus, culpa deleniti, beatae autem, rem suscipit earum!</p>
+          </Modal>
+        </>
     )
 }
